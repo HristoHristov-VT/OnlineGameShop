db19025b4209a0a12d1b38aecf8c1ad49afd0f1a
richvt <hristovt@abv.bg>
2017-06-01 20:22:29 +0300

    Initialize empty ASP.NET CORE Project

---
+++ OnlineGameShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineGameShop.Startup))]
+namespace OnlineGameShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
